1362ab23a0341bfb429fc1f6e71ea9fb21510b2a
Mattias Haal <mattias.haal@grandpublic.se>
2017-11-11 18:06:12 +0100

    Added src

---
+++ src/render.js
@@ -0,0 +1,41 @@
+/**
+ * RENDER
+ * Render the view with requestAnimationFrame on specified event.
+ *
+ * @param {event} type Listen for specified event
+ * @param {node} elem Element to check the event against
+ * @param {function} callback Function to run as a callback
+ * @param {bool} capture Propagating event
+ */
+
+(function (root) {
+
+	'use strict';
+
+	const RENDER = (type, elem, callback, capture) => {
+		const animate = window.requestAnimationFrame;
+		let ticking = false;
+
+		// Only call animate when update is ready:
+		const requestRender = () => {
+			if (!ticking) {
+				animate(update);
+				ticking = true;
+			}
+		};
+
+		// Update the render callback:
+		const update = () => {
+			callback();
+
+			// Reset the tick, so we can capture the next event with requestRender:
+			ticking = false;
+		};
+
+		// Listen for specified event:
+		elem.addEventListener(type, requestRender, !!capture);
+	}
+
+	root.render = RENDER;
+
+})(window);
